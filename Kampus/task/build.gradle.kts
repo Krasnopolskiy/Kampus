a040333da9989dbbac3b08a5cde38d951dec30cd
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2023-09-28 17:02:21 +0300

    ✨ Add stage2 solution

--- Kampus/task/build.gradle.kts
+++ Kampus/task/build.gradle.kts
@@ -1,10 +1,12 @@
 val ktor_version: String by project
+val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm")
-    id("io.ktor.plugin") version "2.3.4"
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin")
 }
 
 repositories {
@@ -15,8 +17,14 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
 }
--- Kampus/task/src/student/build.gradle.kts
+++ Kampus/task/src/student/build.gradle.kts
@@ -1,9 +1,11 @@
 val ktor_version: String by project
+val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
     id("io.ktor.plugin")
 }
 
@@ -25,6 +27,13 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
